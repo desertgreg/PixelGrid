89f109a6b9483328ca95ffe2ec887848b5160b6c
Greg Hjelstrom <greg.hjelstrom@gmail.com>
2021-10-10 11:39:42 -0700

    Implemented BinaryToSnd tool for embedding sounds in cpp code.

---
+++ Tools/BinaryToSnd/BinaryToSnd/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace BinaryToCpp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.FileListView = new System.Windows.Forms.ListView();
+            this.OutputFileTextBox = new System.Windows.Forms.TextBox();
+            this.SelectFilesButton = new System.Windows.Forms.Button();
+            this.SelectOutputButton = new System.Windows.Forms.Button();
+            this.ProcessFiles = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "raw files | *.raw";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // FileListView
+            // 
+            this.FileListView.HideSelection = false;
+            this.FileListView.Location = new System.Drawing.Point(12, 52);
+            this.FileListView.Name = "FileListView";
+            this.FileListView.Size = new System.Drawing.Size(546, 347);
+            this.FileListView.TabIndex = 0;
+            this.FileListView.UseCompatibleStateImageBehavior = false;
+            this.FileListView.View = System.Windows.Forms.View.List;
+            // 
+            // OutputFileTextBox
+            // 
+            this.OutputFileTextBox.Location = new System.Drawing.Point(12, 461);
+            this.OutputFileTextBox.Name = "OutputFileTextBox";
+            this.OutputFileTextBox.Size = new System.Drawing.Size(546, 22);
+            this.OutputFileTextBox.TabIndex = 1;
+            // 
+            // SelectFilesButton
+            // 
+            this.SelectFilesButton.Location = new System.Drawing.Point(12, 23);
+            this.SelectFilesButton.Name = "SelectFilesButton";
+            this.SelectFilesButton.Size = new System.Drawing.Size(113, 23);
+            this.SelectFilesButton.TabIndex = 2;
+            this.SelectFilesButton.Text = "Select Files";
+            this.SelectFilesButton.UseVisualStyleBackColor = true;
+            this.SelectFilesButton.Click += new System.EventHandler(this.SelectFilesButton_Click);
+            // 
+            // SelectOutputButton
+            // 
+            this.SelectOutputButton.Location = new System.Drawing.Point(12, 432);
+            this.SelectOutputButton.Name = "SelectOutputButton";
+            this.SelectOutputButton.Size = new System.Drawing.Size(126, 23);
+            this.SelectOutputButton.TabIndex = 3;
+            this.SelectOutputButton.Text = "Select Output File";
+            this.SelectOutputButton.UseVisualStyleBackColor = true;
+            this.SelectOutputButton.Click += new System.EventHandler(this.SelectOutputButton_Click);
+            // 
+            // ProcessFiles
+            // 
+            this.ProcessFiles.Location = new System.Drawing.Point(441, 515);
+            this.ProcessFiles.Name = "ProcessFiles";
+            this.ProcessFiles.Size = new System.Drawing.Size(117, 23);
+            this.ProcessFiles.TabIndex = 4;
+            this.ProcessFiles.Text = "Generate";
+            this.ProcessFiles.UseVisualStyleBackColor = true;
+            this.ProcessFiles.Click += new System.EventHandler(this.ProcessFiles_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "cpp";
+            this.saveFileDialog1.Filter = "CPP Files | *.cpp";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 550);
+            this.Controls.Add(this.ProcessFiles);
+            this.Controls.Add(this.SelectOutputButton);
+            this.Controls.Add(this.SelectFilesButton);
+            this.Controls.Add(this.OutputFileTextBox);
+            this.Controls.Add(this.FileListView);
+            this.Name = "Form1";
+            this.Text = "BinaryToSnd";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListView FileListView;
+        private System.Windows.Forms.TextBox OutputFileTextBox;
+        private System.Windows.Forms.Button SelectFilesButton;
+        private System.Windows.Forms.Button SelectOutputButton;
+        private System.Windows.Forms.Button ProcessFiles;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Tools/BinaryToSnd/BinaryToSnd/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BinaryToCpp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectFilesButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    FileListView.Clear();
+
+                    foreach(string s in openFileDialog1.FileNames)
+                    {
+                        FileListView.Items.Add(s);
+                    }
+                } 
+                catch (Exception)
+                {
+                }
+            }
+        }
+
+        private void SelectOutputButton_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                OutputFileTextBox.Text = saveFileDialog1.FileName;  
+            }
+        }
+
+
+        void Write_File_Preamble(CodeGenContext con,string filename)
+        {
+            con.sbcpp.Append("#include \"" + Path.GetFileName(con.Header_File_From_Cpp()) + "\"\r\n\r\n");
+
+            // Header
+            con.sbh.Append("#ifndef " + con.Header_Guard() + "\r\n");
+            con.sbh.Append("#define " + con.Header_Guard() + "\r\n\r\n");
+
+            con.sbh.Append("#include \"PGSounds.h\"\r\n\r\n");
+        }
+
+        void Write_Sound_Preamble(CodeGenContext con,string soundname)
+        {
+            con.sbcpp.Append("static const uint8_t " + con.Array_Name(soundname) + "[] = {\r\n");
+            con.sbh.Append("extern PGSound " + con.Obj_Name(soundname) + ";\r\n");
+        }
+
+        private void ProcessFiles_Click(object sender, EventArgs e)
+        {
+            string filename = OutputFileTextBox.Text;
+            CodeGenContext con = new CodeGenContext(filename);
+
+            Write_File_Preamble(con,filename);
+            
+            foreach (ListViewItem item in FileListView.Items)
+            {
+                string soundname = item.Text;
+
+                // Preamble for each file
+                Write_Sound_Preamble(con, soundname);
+                
+                
+                // open the file and read into memory
+                byte[] filedata = File.ReadAllBytes(soundname);
+
+                int bytecounter = 0;
+                int rowcounter = 0;
+                con.sbcpp.Append("\r\n\t");
+                foreach (byte b in filedata)
+                {
+                    con.sbcpp.Append("0x" + Convert.ToString(b, 16).PadLeft(2, ' '));
+
+                    // comma after every byte unless last byte
+                    bytecounter++;
+                    if (bytecounter < filedata.Length)
+                    {
+                        con.sbcpp.Append(",");
+                    }
+
+                    // carriage return after every 'n' charactersc++
+                    rowcounter++;
+                    if (rowcounter > 60)
+                    {
+                        con.sbcpp.Append("\r\n\t");
+                        rowcounter = 0;
+                    }
+                }
+                con.sbcpp.Append("\r\n");
+
+                Write_Sound_Postamble(con, soundname, filedata.Length);
+            }
+            Write_File_Postamble(con, filename);
+
+            con.Generate_Files();
+
+            MessageBox.Show("Success! Generated Files:\r\n" + con.OutputCppName + "\r\n" + con.Header_File_From_Cpp());
+        }
+        void Write_Sound_Postamble(CodeGenContext con,string soundname,int bytes)
+        {
+            con.sbcpp.Append("};\r\n");
+            con.sbcpppost.Append("PGSound " + con.Obj_Name(soundname) + "(" + con.Array_Name(soundname) + ",sizeof(" + con.Array_Name(soundname) + "));\r\n");
+        }
+        void Write_File_Postamble(CodeGenContext con,string filename)
+        {
+            // write all the object declarations at the bottom of the file.
+            con.sbcpp.Append("\r\n\r\n");
+            con.sbcpp.Append(con.sbcpppost);
+            con.sbcpp.Append("\r\n\r\n");
+            con.sbcpp.Append("\r\n\r\n");
+
+            // Header file
+            con.sbh.Append("\r\n#endif //" + con.Header_Guard() + "\r\n");
+        }
+    }
+}
